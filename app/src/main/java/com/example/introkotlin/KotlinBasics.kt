344115ffb90aeb31102f0d258d95932799ced663
Bakyt <djumabaevb@gmail.com>
2021-04-24 10:43:34 +0600

    when statement

--- app/src/main/java/com/example/introkotlin/KotlinBasics.kt
+++ app/src/main/java/com/example/introkotlin/KotlinBasics.kt
@@ -1,6 +1,6 @@
 package com.example.introkotlin
 
-fun main() {
+/*fun main() {
     //TODO: add new functionality
     val world = "world!!!!"
     var age  = 35;
@@ -15,6 +15,28 @@ fun main() {
     val result:Double = a/b
     println(result)
     println("is grater than  + ${5<3}")
+    var num = 35
+    print("num is $num")
+    print("num is ${num++}")
+    print("num is ${++num}")*/
+fun main() {
+
+var season = 3
+    when(season) {
+        1 -> println("spring")
+        2 -> println("summer")
+        3 -> { println("")
+            println("winter") }
+        else -> println("invalid season")
+    }
+    var month = 3
+    when(month) {
+        in 2..3 -> println("spring")
+        in 4..9 -> println("summer")
+        in 12 downTo 1 -> println("winter")
+        12, 1, 2 -> println("winter")
+
+    }
 
 }
 
