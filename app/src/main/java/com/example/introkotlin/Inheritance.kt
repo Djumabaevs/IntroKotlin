83bfac28f8f5df98ac686540090779dce942fe85
Bakyt <djumabaevb@gmail.com>
2021-04-25 12:07:46 +0600

    open -> override and class Any

--- app/src/main/java/com/example/introkotlin/Inheritance.kt
+++ app/src/main/java/com/example/introkotlin/Inheritance.kt
@@ -2,19 +2,25 @@ package com.example.introkotlin
 
 open class Vehicle(val name: String, val brand: String) {
 
-    var range: Double = 0.0
+    open var range: Double = 0.0
     fun extendRange(amount: Double) {
         if(amount > 0) {
             range += amount
         }
     }
-    fun drive(distance: Double) {
+    open fun drive(distance: Double) {
         println("Drove for $distance KM")
     }
 
 }
 class Cars(name: String, brand: String, batteryLife: Double): Vehicle(name, brand) {
-
+    override var range = batteryLife * 6
+    override fun drive(distance: Double) {
+        println("Drove for $distance KM on electricity")
+    }
+    fun drive() {
+        println("Drove for $range KM on electricity")
+    }
 }
 
 //class ElectricCar : Cars() {    cannot inherit from sealed class
