2276437e67a8c8ae064ec602f5270ac589f161b3
Bakyt <djumabaevb@gmail.com>
2021-04-25 12:37:20 +0600

    interfaces

--- app/src/main/java/com/example/introkotlin/Inheritance.kt
+++ app/src/main/java/com/example/introkotlin/Inheritance.kt
@@ -1,6 +1,6 @@
 package com.example.introkotlin
 
-open class Vehicle(val name: String, val brand: String) {
+open class Vehicle(override val maxSpeed: Double, name: String, val brand: String): Drivable {
 
     open var range: Double = 0.0
     fun extendRange(amount: Double) {
@@ -12,15 +12,20 @@ open class Vehicle(val name: String, val brand: String) {
         println("Drove for $distance KM")
     }
 
+    override fun drive(): String = "driving interface"
+
+    /*override fun drive(): String {
+        return "driving interface"
+    }*/
+
 }
-class Cars(name: String, brand: String, batteryLife: Double): Vehicle(name, brand) {
+class Cars(maxSpeed: Double, name: String, brand: String, batteryLife: Double): Vehicle(maxSpeed, name, brand) {
     override var range = batteryLife * 6
     override fun drive(distance: Double) {
         println("Drove for $distance KM on electricity")
     }
-    fun drive() {
-        println("Drove for $range KM on electricity")
-    }
+
+    override fun drive(): String = "drove for $range km on electricity"
 }
 
 //class ElectricCar : Cars() {    cannot inherit from sealed class
@@ -34,6 +39,14 @@ fun main() {
     car.drive(90.7)
     eCar.drive(76.7)
 
+}
+
+interface Drivable {
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake() {
+        println("Drivable is braking")
+    }
 }
 
 
