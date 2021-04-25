efb7437293c873ba782a9cc87fe30ec092f4f585
Bakyt <djumabaevb@gmail.com>
2021-04-25 14:03:49 +0600

    abstract classes

--- app/src/main/java/com/example/introkotlin/Inheritance.kt
+++ app/src/main/java/com/example/introkotlin/Inheritance.kt
@@ -26,6 +26,10 @@ class Cars(maxSpeed: Double, name: String, brand: String, batteryLife: Double): 
     }
 
     override fun drive(): String = "drove for $range km on electricity"
+    override fun brake() {
+        super.brake()
+        println("braking is cool")
+    }
 }
 
 //class ElectricCar : Cars() {    cannot inherit from sealed class
@@ -33,11 +37,15 @@ class Cars(maxSpeed: Double, name: String, brand: String, batteryLife: Double): 
 //}
 
 fun main() {
-    var car = Vehicle("LC200", "Toyota")
-    var eCar = Cars("T-300", "Tesla", 100.0)
+    var car = Vehicle(300.0,"LC200", "Toyota")
+    var eCar = Cars(340.5, "T-300", "Tesla", 100.0)
 
     car.drive(90.7)
     eCar.drive(76.7)
+    println("-------------------------")
+    eCar.brake()
+    println("-------------------------")
+    car.brake()
 
 }
 
@@ -46,7 +54,29 @@ interface Drivable {
     fun drive(): String
     fun brake() {
         println("Drivable is braking")
+      }
+    }
+abstract class Mammal(private val name: String, private val origin: String, private val weight: Double) {
+    abstract var maxSpeed: Double //must be overridden by subclasses
+    abstract fun breath() //must be implemented by subclasses
+    abstract fun run()
+
+    //concrete method
+    fun display() {
+        println("max speed: $maxSpeed +  name: $name, origin: $origin, weight: $weight ")
     }
 }
+
+class Human(name: String, origin: String, weight: Double, override var maxSpeed: Double): Mammal(name, origin, weight) {
+
+    override fun breath() {
+        println("breathe every second")
+    }
+
+    override fun run() {
+        println("run on two legs")
+    }
+}
+
 
 
